a0c6643c33ad71439787fdfe612d350094946a91
Enxos27 <vincenzo.calvaruso17@gmail.com>
2026-03-02 11:53:48 +0100

    Initial Spring Boot project scaffold

---
+++ src/main/java/vincenzocalvaruso/Heritage_Kitchen/HeritageKitchenApplication.java
@@ -0,0 +1,13 @@
+package vincenzocalvaruso.Heritage_Kitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeritageKitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeritageKitchenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vincenzocalvaruso/Heritage_Kitchen/HeritageKitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package vincenzocalvaruso.Heritage_Kitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeritageKitchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
